3c5d6bc65d58ced91b55deb71ed34da3b0a942d3
Wes Matlock <thomasmatlockbba@gmail.com>
2024-09-09 20:49:27 -0500

    Update lesson

--- RollWithIt/Models/Lessons/Lesson.cs
+++ RollWithIt/Models/Lessons/Lesson.cs
@@ -8,10 +8,17 @@ namespace RollWithIt.Models.Lessons
         [Key]
         [JsonPropertyName("id")] // Ensure that this matches the JSON response
         public int Id { get; set; }
-
         [Required]
         [JsonPropertyName("title")] // Ensure this matches the JSON response
         public string Title { get; set; } = string.Empty;
+        [JsonPropertyName("belt")]
+        public string Belt {  get; set; }
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+        [JsonPropertyName("difficulty")]
+        public string Difficulty { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
 
         [JsonPropertyName("lessonSections")]
         public List<LessonSection>? LessonSections { get; set; }
